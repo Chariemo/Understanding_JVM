b046c3ff38dbc9663b4cf6d82eb9e2abb6a4ade8
Chariemo <charleyschen@163.com>
2017-06-26 15:22:27 +0800

    runtime constant pool OOM

--- src/main/java/OOM/RuntimeConstantPoolOOM.java
+++ src/main/java/OOM/RuntimeConstantPoolOOM.java
@@ -19,11 +19,5 @@ public class RuntimeConstantPoolOOM {
             list.add(String.valueOf(i++).intern());
         }
 
-     /*  String str1 = new StringBuilder("计算机").append("软件").toString();
-       System.out.println(str1.intern() == str1);
-
-       String str2 = new StringBuilder("java").toString();
-        System.out.println(str2.intern() == str2);*/
-
     }
 }
